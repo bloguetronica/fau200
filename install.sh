92da4d9fd0fe678731736af9e79371fd87f3eb03
Samuel Lourenço <16304376+samuelfmlourenco@users.noreply.github.com>
2022-06-05 12:36:14 +0100

    Add files via upload

--- install.sh
+++ install.sh
@@ -5,31 +5,35 @@ apt-get -qq update
 apt-get -qq install build-essential
 apt-get -qq install libusb-1.0-0-dev
 echo Copying source code files...
-mkdir -p /usr/local/src/fau200
-cp -f src/common.c /usr/local/src/fau200/.
-cp -f src/common.h /usr/local/src/fau200/.
-cp -f src/fau200-clear.c /usr/local/src/fau200/.
-cp -f src/fau200-core.c /usr/local/src/fau200/.
-cp -f src/fau200-core.h /usr/local/src/fau200/.
-cp -f src/fau200-list.c /usr/local/src/fau200/.
-cp -f src/fau200-lockotp.c /usr/local/src/fau200/.
-cp -f src/fau200-reset.c /usr/local/src/fau200/.
-cp -f src/fau200-volt.c /usr/local/src/fau200/.
+mkdir -p /usr/local/src/fau200/man
+cp -f src/cp2130.cpp /usr/local/src/fau200/.
+cp -f src/cp2130.h /usr/local/src/fau200/.
+cp -f src/error.cpp /usr/local/src/fau200/.
+cp -f src/error.h /usr/local/src/fau200/.
+cp -f src/fau200-clear.cpp /usr/local/src/fau200/.
+cp -f src/fau200device.cpp /usr/local/src/fau200/.
+cp -f src/fau200device.h /usr/local/src/fau200/.
+cp -f src/fau200-info.cpp /usr/local/src/fau200/.
+cp -f src/fau200-list.cpp /usr/local/src/fau200/.
+cp -f src/fau200-lockotp.cpp /usr/local/src/fau200/.
+cp -f src/fau200-reset.cpp /usr/local/src/fau200/.
+cp -f src/fau200-volt.cpp /usr/local/src/fau200/.
 cp -f src/GPL.txt /usr/local/src/fau200/.
 cp -f src/LGPL.txt /usr/local/src/fau200/.
 cp -f src/libusb-extra.c /usr/local/src/fau200/.
 cp -f src/libusb-extra.h /usr/local/src/fau200/.
 cp -f src/Makefile /usr/local/src/fau200/.
+cp -f src/man/fau200-clear.1 /usr/local/src/fau200/man/.
+cp -f src/man/fau200-info.1 /usr/local/src/fau200/man/.
+cp -f src/man/fau200-list.1 /usr/local/src/fau200/man/.
+cp -f src/man/fau200-lockotp.1 /usr/local/src/fau200/man/.
+cp -f src/man/fau200-reset.1 /usr/local/src/fau200/man/.
+cp -f src/man/fau200-volt.1 /usr/local/src/fau200/man/.
 cp -f src/README.txt /usr/local/src/fau200/.
-echo Building and installing binaries...
-make -C /usr/local/src/fau200 all install clean
-echo Installing man pages...
-mkdir -p /usr/local/share/man/man1
-cp -f man/fau200-clear.1.gz /usr/local/share/man/man1/.
-cp -f man/fau200-list.1.gz /usr/local/share/man/man1/.
-cp -f man/fau200-lockotp.1.gz /usr/local/share/man/man1/.
-cp -f man/fau200-reset.1.gz /usr/local/share/man/man1/.
-cp -f man/fau200-volt.1.gz /usr/local/share/man/man1/.
+cp -f src/utils.cpp /usr/local/src/fau200/.
+cp -f src/utils.h /usr/local/src/fau200/.
+echo Building and installing binaries and man pages...
+make -C /usr/local/src/fau200 install clean
 echo Applying configurations...
 cat > /etc/udev/rules.d/70-bgtn-fau200.rules << EOF
 SUBSYSTEM=="usb", ATTRS{idVendor}=="10c4", ATTRS{idProduct}=="8c35", MODE="0666"
